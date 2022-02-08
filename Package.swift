4fac628f9595cff1167c5f4ebd35931a40c66753
Caner Y. Tatar <caner.tatar@depop.com>
2022-02-08 11:33:54 +0000

    Type is reverted

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "InputBarAccessoryView",
-            type: .static,
             targets: ["InputBarAccessoryView"])
     ],
     targets: [

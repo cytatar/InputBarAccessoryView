9a62080f1ce35533657c4f7f146a32999e10279a
Caner Y. Tatar <caner.tatar@depop.com>
2022-02-08 11:28:17 +0000

    Type is changed to static

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "InputBarAccessoryView",
-            type: .dynamic,
+            type: .static,
             targets: ["InputBarAccessoryView"])
     ],
     targets: [
